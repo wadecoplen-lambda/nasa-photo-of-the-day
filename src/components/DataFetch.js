95c6970e2071da763b3e777143c4b2b647dfab9f
Wade-Coplen <wadecoplen@gmail.com>
2020-08-11 11:30:22 -0700

    data fetch and parsed

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -1,11 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-
 function DataFetch() {
-    const [data, setData] = useData([])
+    const [data, setData] = useState([])
     useEffect(() => {
         axios
-            .get('http:api.nasa.gov/planetary/apod?api_key=y7LVal7n67ymFfeAnFPKo73dUE8wIp3J4WXllqfW')
+            .get('https://api.nasa.gov/planetary/apod?api_key=y7LVal7n67ymFfeAnFPKo73dUE8wIp3J4WXllqfW')
             .then(res => {
                 console.log('I am the response', res)
             })
